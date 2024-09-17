bad2b337d1a4b13d2ddda5ea31a29cbb9b7f4eda
Magne Rayan <r.magne@lprs.fr>
2024-09-17 14:50:31 +0200

    feat : mise en place de l'interface de connexion et test des elements de la methode du controller

--- src/main/java/appli/acceuil/LoginController.java
+++ src/main/java/appli/acceuil/LoginController.java
@@ -1,14 +1,37 @@
 package appli.acceuil;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        @FXML
+        private TextField emailField;
+
+        @FXML
+        private PasswordField passwordField;
+
+
+        @FXML
+        void connexion(ActionEvent event) {
+
+            System.out.println(emailField.getText());
+            System.out.println(passwordField.getText());
+
+        }
+
+        @FXML
+        void inscription(ActionEvent event) {
+
+        }
+
+        @FXML
+        void motDePasseOublie(ActionEvent event) {
+
+        }
+
     }
-}
+
