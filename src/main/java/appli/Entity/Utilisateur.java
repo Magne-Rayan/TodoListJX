6043c7312fdbc6b2c89b5671a057806cbdf4d96c
Magne Rayan <r.magne@lprs.fr>
2024-10-01 14:15:11 +0200

    Feat : creation entity Utilisateur -> getter et setter, constructeur

---
+++ src/main/java/appli/Entity/Utilisateur.java
@@ -0,0 +1,63 @@
+package appli.Entity;
+
+public class Utilisateur {
+
+
+    private int id_utilisateur;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mot_de_passe;
+
+    public Utilisateur(int id_utilisateur, String nom, String prenom, String email, String mot_de_passe) {
+        this.id_utilisateur = id_utilisateur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mot_de_passe = mot_de_passe;
+    }
+
+
+    public int getId_utilisateur() {
+        return id_utilisateur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMot_de_passe() {
+        return mot_de_passe;
+    }
+
+
+    public void setId_utilisateur(int id_utilisateur) {
+        this.id_utilisateur = id_utilisateur;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMot_de_passe(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
+
+
+}
